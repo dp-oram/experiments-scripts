b23a704905dfc3e456a229f625dfce6137262a20
Dmytro Bogatov <dmytro@dbogatov.org>
2019-06-16 18:05:44 -0400

    Add Oracle to the bench.

--- scripts/uniform-range-queries.py
+++ scripts/uniform-range-queries.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-from dotmap import DotMap
 import random
 import time
 
@@ -10,18 +9,19 @@ def parse():
 	inputSizeDefault = 100
 	inputSizeMin = 10
 	inputSizeMax = 100000
+
 	def argcheckInputSize(value):
 		number = int(value)
 		if number < inputSizeMin or number > inputSizeMax:
 			raise argparse.ArgumentTypeError(f"Input / queries size must be {inputSizeMin} to {inputSizeMax}. Given {number}")
 		return number
-		
+
 	def argcheckInputMax(value):
 		number = int(value)
 		if number <= 0:
 			raise argparse.ArgumentTypeError(f"Input max must be above 0. Given {number}")
 		return number
-		
+
 	def argcheckRange(value):
 		percent = float(value)
 		if percent < 0.01 or percent > 0.9:
@@ -36,65 +36,108 @@ def parse():
 	parser.add_argument("--seed", dest="seed", metavar="seed", type=int, default=123456, required=False, help="Seed to use for PRG")
 	parser.add_argument("--kalepso-port", dest="kalepsoPort", metavar="kalepso-port", type=int, default=3306, required=False, help="Kalepso port")
 	parser.add_argument("--mysql-port", dest="mysqlPort", metavar="mysql-port", type=int, default=3307, required=False, help="MySQL port")
+	parser.add_argument("--oracle-port", dest="oraclePort", metavar="oracle-port", type=int, default=1521, required=False, help="Oracle port")
 
 	args = parser.parse_args()
-	
+
 	random.seed(args.seed)
 
-	return DotMap({"size": args.size, "max": args.max, "range": args.range, "queries": args.queries, "kalepsoPort": args.kalepsoPort, "mysqlPort": args.mysqlPort})
+	return args.size, args.max, args.range, args.queries, args.kalepsoPort, args.mysqlPort, args.oraclePort
 
-def generateLoads(inputs):
+
+def generateLoads(dataSize, maxValue, queryRange, queriesSize):
 	data = []
 	queries = []
-	for i in range(1, inputs.size):
-		data += [random.randint(0, inputs.max)]
-	
-	querySize = int(inputs.max * inputs.range)
-	for i in range(1, inputs.queries):
-		left = random.randint(0, inputs.max - querySize)
-		queries += [(left, left + querySize)]
-	
-	return DotMap({"data": data, "queries": queries})
+	for i in range(1, dataSize):
+		data += [random.randint(0, maxValue)]
 
-def runLoads(loads, port):
+	querySize = int(maxValue * queryRange)
+	for i in range(1, queriesSize):
+		left = random.randint(0, maxValue - querySize)
+		queries += [(left, left + querySize)]
+
+	return data, queries
+
+
+def runLoadsMySQL(data, queries, port):
 	import mysql.connector as mysql
-	
+
 	db = mysql.connect(
-		host = "localhost",
-		user = "root",
-		port = port,
-		passwd = "MYPASSWD"
+		host="localhost",
+		user="root",
+		port=port,
+		passwd="MYPASSWD"
 	)
 
 	cursor = db.cursor()
 	cursor.execute("CREATE DATABASE IF NOT EXISTS experimental")
 	cursor.execute("USE experimental")
 	cursor.execute("DROP TABLE IF EXISTS ranges")
-	cursor.execute("CREATE TABLE IF NOT EXISTS ranges (number INT, INDEX numberIndex (number))")
+	cursor.execute("CREATE TABLE IF NOT EXISTS ranges (point INT, INDEX pointIndex (point))")
 	cursor.execute("SET SESSION query_cache_type=0")
+
+	db.commit()
 
 	init = time.time()
 
-	insert = "INSERT INTO ranges (number) VALUES (%s)"
-	for point in loads.data:
+	insert = "INSERT INTO ranges (point) VALUES (%s)"
+	for point in data:
 		cursor.execute(insert, (point,))
 		db.commit()
 
 	inserted = time.time()
-	
-	query = "SELECT number FROM ranges WHERE number BETWEEN %s AND %s"
-	for rangeQuery in loads.queries:
+
+	query = "SELECT point FROM ranges WHERE point BETWEEN %s AND %s"
+	for rangeQuery in queries:
 		cursor.execute(query, rangeQuery)
 		records = cursor.fetchall()
 
 	queried = time.time()
-	
+
 	return DotMap({"insertion": inserted - init, "queries": queried - inserted})
 
+
+def runLoadsOracle(data, queries, port):
+	import cx_Oracle
+
+	connection = cx_Oracle.connect("dmytro", "password", f"0.0.0.0:{port}/ORCLCDB.localdomain")
+
+	cursor = connection.cursor()
+
+	cursor.execute("BEGIN EXECUTE IMMEDIATE 'DROP TABLE ranges'; EXCEPTION WHEN OTHERS THEN NULL; END;")
+	cursor.execute("CREATE TABLE ranges (point NUMBER ENCRYPT NO SALT)")
+	cursor.execute("CREATE INDEX pointIndex on ranges (point)")
+
+	connection.commit()
+
+	init = time.time()
+
+	cursor.execute("INSERT INTO employee (first_name, last_name, empID, salary) VALUES ('John', 'Doe', 1, 152)")
+
+	insert = "INSERT INTO ranges (point) VALUES (:point)"
+	for point in data:
+		cursor.execute(insert, point=point)
+		connection.commit()
+
+	inserted = time.time()
+
+	query = "SELECT point FROM ranges WHERE point BETWEEN :low AND :high"
+	for rangeQuery in queries:
+		cursor.execute(query, low=rangeQuery[0], high=rangeQuery[1])
+		records = cursor.fetchall()
+		# print(f"For range {rangeQuery}, the result is {len(records)} records")
+
+	queried = time.time()
+
+	return inserted - init, queried - inserted
+
+
 if __name__ == "__main__":
-	inputs = parse()
-	loads = generateLoads(inputs)
-	
-	for engine in [("MySQL", inputs.mysqlPort), ("Kelepso", inputs.kalepsoPort)]:
-		times = runLoads(loads, engine[1])
-		print(f"For {engine[0]}: inserted in {int(times.insertion * 1000)} ms, queries in {int(times.queries * 1000)} ms")
+
+	dataSize, maxValue, queryRange, queriesSize, mysqlPort, kalepsoPort, oraclePort = parse()
+	data, queries = generateLoads(dataSize, maxValue, queryRange, queriesSize)
+
+	for engine in [("Oracle", oraclePort, runLoadsOracle), ("MySQL", mysqlPort, runLoadsMySQL), ("Kelepso", kalepsoPort, runLoadsMySQL)]:
+	# for engine in [("Oracle", oraclePort, runLoadsOracle)]:
+		insertionTime, queryTime = engine[2](data, queries, engine[1])
+		print(f"For {engine[0]}: inserted in {int(insertionTime * 1000)} ms, queries in {int(queryTime * 1000)} ms")
