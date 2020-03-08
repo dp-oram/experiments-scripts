8c7212764219144334d5d4f5a04292844e0799d6
Dmytro Bogatov <dmytro@dbogatov.org>
2020-03-07 22:01:01 -0500

    Add real dataset for mysql loads.

--- scripts/uniform-range-queries.py
+++ scripts/uniform-range-queries.py
@@ -2,20 +2,21 @@
 
 import random
 import time
+import math
 from enum import Enum, auto
 
 
 class Engine(Enum):
-	kalepso = auto()
-	mariadb = auto()
-	oracle = auto()
-	microsoft = auto()
+	kalepso = 3306
+	mariadb = 3307
+	oracle = 1521
+	microsoft = 1433
 
 	def __str__(self):
 		return self.name
 
 	@staticmethod
-	def value(key):
+	def valueForParse(key):
 		try:
 			return Engine[key]
 		except KeyError:
@@ -59,38 +60,30 @@ def parse():
 
 	parser.add_argument("--seed", dest="seed", metavar="seed", type=int, default=123456, required=False, help="Seed to use for PRG")
 
-	parser.add_argument('--engine', dest="engine", metavar="engine", type=Engine.value, choices=list(Engine), required=True, help="Engine to run benchmark against")
-	parser.add_argument("--port", dest="port", metavar="port", type=int, required=False, help="Engine's port (not required for MS)")
+	parser.add_argument('--engine', dest="engine", metavar="engine", type=Engine.valueForParse, choices=list(Engine), required=True, help="Engine to run benchmark against")
 
 	args = parser.parse_args()
 
-	if args.engine != Engine.microsoft and not args.port:
-		parser.error('--port is required when --engine is not microsoft.')
-
 	random.seed(args.seed)
 
-	return args.size, args.range, args.queries, args.engine, args.port
+	return args.size, args.range, args.queries, args.engine
 
 
 def generateLoads(dataSize, queryRange, queriesSize):
 	import pandas as pd
 
-	data = pd.read_csv("https://gist.githubusercontent.com/dbogatov/a192d00d72de02f188c5268ea1bbf25b/raw/afb4d44111f42dbe9ed0148bee3b954d43e38907/state-of-california-2017.csv")
+	data = pd.read_csv("https://gist.githubusercontent.com/dbogatov/a192d00d72de02f188c5268ea1bbf25b/raw/b1e7ea9e058e7906e0045b29ad75a5f201bd4f57/state-of-california-2019.csv")
 	# data = pd.read_csv("data.csv")
 	
-	print(data)
+	sampled = data.sample(frac = float(dataSize) / len(data.index))
 
 	queries = []
-	# for i in range(1, dataSize):
-	# 	data += [random.randint(0, maxValue)]
+	
+	for i in range(1, queriesSize):
+		left = random.randint(0, int(sampled["Total Pay & Benefits"].max()) - queryRange)
+		queries += [(left, left + queryRange)]
 
-	# querySize = int(maxValue * queryRange)
-	# for i in range(1, queriesSize):
-	# 	left = random.randint(0, maxValue - querySize)
-	# 	queries += [(left, left + querySize)]
-
-	return data, queries
-
+	return sampled, queries
 
 def runLoadsMySQL(data, queries, port):
 	import mysql.connector as mysql
@@ -99,35 +92,90 @@ def runLoadsMySQL(data, queries, port):
 		host="localhost",
 		user="root",
 		port=port,
-		passwd="MYPASSWD"
+		passwd="kalepso"
 	)
 
 	cursor = db.cursor()
-	cursor.execute("CREATE DATABASE IF NOT EXISTS experimental")
-	cursor.execute("USE experimental")
-	cursor.execute("DROP TABLE IF EXISTS ranges")
-	cursor.execute("CREATE TABLE IF NOT EXISTS ranges (point INT, INDEX pointIndex (point))")
+	cursor.execute("CREATE DATABASE IF NOT EXISTS CA_public_employees_salaries_2019")
+	cursor.execute("USE CA_public_employees_salaries_2019")
+	cursor.execute("DROP TABLE IF EXISTS salaries")
+	cursor.execute("""
+		CREATE TABLE IF NOT EXISTS salaries (
+			fullname VARCHAR(100),		# Employee Name
+			jobtitle VARCHAR(100),		# Job Title
+			salary FLOAT,				# Base Pay
+			overtimepay FLOAT,			# Overtime Pay
+			other FLOAT,				# Other Pay
+			benefits FLOAT NULL,		# Benefits
+			total FLOAT,				# Total Pay
+			totalPlusBenefits FLOAT,	# Total Pay & Benefits
+			year INT,					# Year
+			notes VARCHAR(150) NULL,	# Notes
+			agency VARCHAR(100),		# Agency
+			status VARCHAR(30),			# Status
+			
+			INDEX totalPlusBenefitsIndex (totalPlusBenefits)
+		)
+	""")
 	cursor.execute("SET SESSION query_cache_type=0")
 
 	db.commit()
 
 	init = time.time()
 
-	insert = "INSERT INTO ranges (point) VALUES (%s)"
-	for point in data:
-		cursor.execute(insert, (point,))
+	insert = """
+		INSERT INTO salaries 
+			(fullname, jobtitle, salary, overtimepay, other, benefits, total, totalPlusBenefits, year, notes, agency, status) 
+		VALUES 
+			(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+	"""
+	for index, record in data.iterrows():
+		cursor.execute(
+			insert,
+			(
+				record["Employee Name"],
+				record["Job Title"],
+				float(record["Base Pay"]),
+				float(record["Overtime Pay"]),
+				float(record["Other Pay"]),
+				0.0 if record["Benefits"] == "Not Provided" else float(record["Benefits"]),
+				float(record["Total Pay"]),
+				float(record["Total Pay & Benefits"]),
+				int(record["Year"]),
+				"" if math.isnan(record["Notes"]) else record["Notes"],
+				record["Agency"],
+				"" if math.isnan(record["Status"]) else record["Status"]
+			)
+		)
 		db.commit()
 
 	inserted = time.time()
 
-	query = "SELECT point FROM ranges WHERE point BETWEEN %s AND %s"
+	query = "SELECT * FROM salaries WHERE totalPlusBenefits BETWEEN %s AND %s"
 	for rangeQuery in queries:
 		cursor.execute(query, rangeQuery)
 		records = cursor.fetchall()
 
 	queried = time.time()
+	
+	sizeQuery = """
+		SELECT
+			ROUND((DATA_LENGTH + INDEX_LENGTH)) AS `Size (B)`
+		FROM
+			information_schema.TABLES
+		WHERE
+			TABLE_SCHEMA = "CA_public_employees_salaries_2019"
+		AND
+			TABLE_NAME = "salaries"
+		ORDER BY
+			(DATA_LENGTH + INDEX_LENGTH)
+		DESC
+	"""
 
-	return inserted - init, queried - inserted
+	cursor.execute(sizeQuery)
+	size = int(cursor.fetchall()[0][0])
+
+	return inserted - init, queried - inserted, size
 
 
 def runLoadsOracle(data, queries, port):
@@ -176,14 +224,14 @@ def generateMSSQLLoad(data, queries):
 
 if __name__ == "__main__":
 
-	dataSize, queryRange, queriesSize, engine, port = parse()
+	dataSize, queryRange, queriesSize, engine = parse()
 	data, queries = generateLoads(dataSize, queryRange, queriesSize)
 
-	# if engine == Engine.microsoft:
-	# 	generateMSSQLLoad(data, queries)
-	# else:
-	# 	if engine == Engine.kalepso or engine == Engine.mariadb:
-	# 		insertionTime, queryTime = runLoadsMySQL(data, queries, port)
-	# 	else:
-	# 		insertionTime, queryTime = runLoadsOracle(data, queries, port)
-	# 	print(f"For {engine}: inserted in {int(insertionTime * 1000)} ms, queries in {int(queryTime * 1000)} ms")
+	if engine == Engine.microsoft:
+		generateMSSQLLoad(data, queries)
+	else:
+		if engine == Engine.kalepso or engine == Engine.mariadb:
+			insertionTime, queryTime, tableSize = runLoadsMySQL(data, queries, engine.value)
+		else:
+			insertionTime, queryTime, tableSize = runLoadsOracle(data, queries, engine.value)
+		print(f"For {engine}: inserted in {int(insertionTime * 1000)} ms, queries in {int(queryTime * 1000)} ms, database size is {tableSize} bytes")
