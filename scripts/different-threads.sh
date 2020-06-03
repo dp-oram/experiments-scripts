fc7d5fe1305f1f39b0bb86a7e77cbe8e33ab992d
Dmytro Bogatov <dmytro@dbogatov.org>
2020-06-03 18:58:31 -0400

    Add script.

---
+++ scripts/different-threads.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+# Ensure that the CWD is set to script's location
+cd "${0%/*}"
+CWD=$(pwd)
+
+for threads in {1..16}
+do
+	# if [ "$threads" -ge "4" ]; then
+	# 	gamma="true"
+	# else
+	# 	gamma="false"
+	# fi
+	echo "Running for $threads threads"
+	cd ../../dp-oram/dp-oram
+	./bin/main -g true -r true -p true -s inmemory -n $threads -b 0 --beta 20 --epsilon 1 --useGamma true --levels 0 -v trace
+done
+
+echo "Done!"
