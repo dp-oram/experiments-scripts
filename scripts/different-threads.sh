eacefa88ad42729e7e0c6558acef1eee265d2146
Dmytro Bogatov <dmytro@dbogatov.org>
2020-06-04 13:38:26 -0400

    Add optimization flag.

--- scripts/different-threads.sh
+++ scripts/different-threads.sh
@@ -8,6 +8,7 @@ cd "${0%/*}"
 CWD=$(pwd)
 
 ORAMS=$1
+OPTIMIZATION=$2
 
 for threads in {1..16}
 do
@@ -18,7 +19,7 @@ do
 	# fi
 	echo "Running for $threads threads"
 	cd ../../dp-oram/dp-oram
-	./bin/main -g true -r true -p true -s inmemory -n $threads -b 4096 -u $ORAMS --beta 20 --epsilon 1 --useGamma true --levels 0 -v trace
+	./bin/main -g true -r true -p true -s inmemory -n $threads -b 4096 -u $ORAMS --beta 20 --epsilon 1 --useGamma true --useOramOptimization $OPTIMIZATION --levels 0 -v trace
 done
 
 echo "Done!"
