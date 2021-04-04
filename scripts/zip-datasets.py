21983abf6978858fb6241fe77b22f83ffb5cbfe8
Dmytro Bogatov <dmytro@dbogatov.org>
2021-04-04 11:04:30 -0400

    Add queryset to ZIP.

--- scripts/zip-datasets.py
+++ scripts/zip-datasets.py
@@ -5,13 +5,17 @@ import random
 import logging
 import os
 
+
 def parse():
 	import argparse
 
 	parser = argparse.ArgumentParser(description="Expand distribution")
 
-	parser.add_argument("--left", dest="left", metavar="left-dataset", type=argparse.FileType("r"), required=True, help=f"The left dataset file.")
-	parser.add_argument("--right", dest="right", metavar="right-dataset", type=argparse.FileType("r"), required=True, help=f"The right dataset file.")
+	parser.add_argument("--left-dataset", dest="leftDataset", metavar="left-dataset", type=argparse.FileType("r"), required=True, help=f"The left dataset file.")
+	parser.add_argument("--right-dataset", dest="rightDataset", metavar="right-dataset", type=argparse.FileType("r"), required=True, help=f"The right dataset file.")
+
+	parser.add_argument("--left-queryset", dest="leftQueryset", metavar="left-queryset", type=argparse.FileType("r"), required=True, help=f"The left queryset file.")
+	parser.add_argument("--right-queryset", dest="rightQueryset", metavar="right-queryset", type=argparse.FileType("r"), required=True, help=f"The right queryset file.")
 
 	parser.add_argument("--seed", dest="seed", metavar="seed", type=int, default=123456, required=False, help="Seed to use for PRG")
 	parser.add_argument("-v", "--verbose", dest="verbose", default=False, help="increase output verbosity", action="store_true")
@@ -27,18 +31,17 @@ def parse():
 	random.seed(args.seed)
 	np.random.seed(args.seed + 1)
 
-	return args.left, args.right
+	return args.leftDataset, args.rightDataset, args.leftQueryset, args.rightQueryset
 
 
 def main():
 
-	left, right = parse()
+	leftDataset, rightDataset, leftQueryset, rightQueryset = parse()
 	cwd = os.path.dirname(os.path.abspath(__file__))
 
 	counter = 0
-
-	with left as leftIn:
-		with right as rightIn:
+	with leftDataset as leftIn:
+		with rightDataset as rightIn:
 			with open(f"{cwd}/../output/dataset-merged.csv", "w") as out:
 				while True:
 					leftLine = leftIn.readline()
@@ -48,7 +51,23 @@ def main():
 					out.write(f"{leftLine.rstrip()},{rightLine.rstrip()}\n")
 					counter += 1
 
-	logging.info(f"Written {counter} lines!")
+	logging.info(f"Written {counter} dataset lines!")
+
+	counter = 0
+	with leftQueryset as leftIn:
+		with rightQueryset as rightIn:
+			with open(f"{cwd}/../output/queryset-merged.csv", "w") as out:
+				while True:
+					leftLine = leftIn.readline()
+					rightLine = rightIn.readline()
+					if not leftLine or not rightLine:
+						break
+					out.write(f"{leftLine.rstrip()}\n")
+					out.write(f"{rightLine.rstrip()}\n")
+					counter += 2
+
+	logging.info(f"Written {counter} queryset lines!")
+
 
 if __name__ == "__main__":
 	main()
